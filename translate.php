9575acccf4c0b227cfe572a6a1dabf25c1185dda
Ivan Shcherbak <alotofall@gmail.com>
2020-11-02 20:20:36 +0200

    Translate files in directories

--- translate.php
+++ translate.php
@@ -8,24 +8,6 @@ use Gettext\Generator\PoGenerator;
 use Gettext\Loader\PoLoader;
 use Gettext\Translation;
 
-
-$client = new TranslateClient([
-    'region' => 'us-west-2',
-    'version' => 'latest',
-    'credentials' => CredentialProvider::ini('default', __DIR__ . '/credentials.ini')
-]);
-
-//import from a .po file:
-$loader = new PoLoader();
-$file = ($argv[1] ?? null);
-if (!is_file($file)) {
-    echo "Invalid file path.\n";
-    exit(2);
-}
-$translations = $loader->loadFile($file);
-$save = false;
-$limit = (int)($argv[2] ?? 10);
-$verbose = true;
 function replace(string $regex, string $original, array &$replacers)
 {
     return preg_replace_callback($regex, function ($data) use (&$replacers) {
@@ -36,48 +18,95 @@ function replace(string $regex, string $original, array &$replacers)
     }, $original);
 }
 
-foreach ($translations as $translation) {
-    assert($translation instanceof Translation);
-    if ($translation->getTranslation() === null || $translation->getTranslation() === '') {
-        $limit--;
-        $replacers = [];
-        $original = $translation->getOriginal();
-        $toTranslate = $original;
-        $toTranslate = replace('!<[^>]+>!', $toTranslate, $replacers);
-        $toTranslate = replace('!\%\{[^\}]+\}!', $toTranslate, $replacers);
-        echo 'EN: ' . $original . "\n";
-        $result = $client->translateText([
-            'SourceLanguageCode' => 'en',
-            'TargetLanguageCode' => 'uk',
-            'Text' => $toTranslate,
-        ]);
-        $result = $result['TranslatedText'];
-        $uk = strtr($result, $replacers);
-        echo 'UK: ' . $uk . "\n";
-        if ($verbose && $uk !== $result) {
-            echo '<<<>>>' . "\n";
-            echo '>>> ->: ' . $toTranslate . "\n";
-            echo '>>> <-: ' . $result . "\n";
+/**
+ * @param string $dir
+ * @return string[]|Generator
+ */
+function find(string $dir): Generator
+{
+    foreach (glob($dir . '/*') as $item) {
+        if (is_dir($item)) {
+            yield from find($dir);
+        } elseif (preg_match('!\.po$!', $item)) {
+            yield $item;
         }
-        echo '~~~~~~~~~~~~~~~~~~~' . "\n";
-        $translation->translate($uk);
-        $save = true;
-        sleep(1);
-    }
-    if ($limit <= 0) {
-        break;
     }
 }
-$translated = 0;
-foreach ($translations as $translation) {
-    assert($translation instanceof Translation);
-    $translated = $translated + (int)$translation->isTranslated();
+
+function translateFile(string $file, int $limit, PoLoader $loader, TranslateClient $client): void
+{
+    echo $file . "\n";
+    $translations = $loader->loadFile($file);
+    $save = false;
+    $verbose = true;
+    foreach ($translations as $translation) {
+        assert($translation instanceof Translation);
+        if ($translation->getTranslation() === null || $translation->getTranslation() === '') {
+            $limit--;
+            $replacers = [];
+            $original = $translation->getOriginal();
+            $toTranslate = $original;
+            $toTranslate = replace('!<[^>]+>!', $toTranslate, $replacers);
+            $toTranslate = replace('!\{[a-z0-9_-\}\{]+\}!i', $toTranslate, $replacers);
+            $toTranslate = replace('!\%\{[^\}]+\}!', $toTranslate, $replacers);
+            echo 'EN: ' . $original . "\n";
+            $result = $client->translateText([
+                'SourceLanguageCode' => 'en',
+                'TargetLanguageCode' => 'uk',
+                'Text' => $toTranslate,
+            ]);
+            $result = $result['TranslatedText'];
+            $uk = strtr($result, $replacers);
+            echo 'UK: ' . $uk . "\n";
+            if ($verbose && $uk !== $result) {
+                echo '<<<~~~~~~~~~~~~~~~~~~>>>' . "\n";
+                echo '>>> ->: ' . $toTranslate . "\n";
+                echo '>>> <-: ' . $result . "\n";
+                echo '^^^^^^ ~~~~~~~~~~~~~~~~~~ //// ' . "\n";
+            }
+            echo '~~~~~~~~~~~~~~~~~~~' . "\n";
+            $translation->translate($uk);
+            $save = true;
+            sleep(1);
+        }
+        if ($limit <= 0) {
+            break;
+        }
+    }
+    $translated = 0;
+    foreach ($translations as $translation) {
+        assert($translation instanceof Translation);
+        $translated = $translated + (int)$translation->isTranslated();
+    }
+    echo 'Translated: ' . $translated . "\n";
+    if ($save) {
+        (new PoGenerator())->generateFile(
+            $translations,
+            $file
+        );
+        echo 'Saved' . "\n";
+    }
 }
-echo 'Translated: ' . $translated . "\n";
-if ($save) {
-    (new PoGenerator())->generateFile(
-        $translations,
-        $file
-    );
-    echo 'Saved' . "\n";
+
+$path = ($argv[1] ?? null);
+$limit = (int)($argv[2] ?? 10);
+
+$loader = new PoLoader();
+$client = new TranslateClient([
+    'region' => 'us-west-2',
+    'version' => 'latest',
+    'credentials' => CredentialProvider::ini('default', __DIR__ . '/credentials.ini')
+]);
+
+if (is_dir($path)) {
+    foreach (find($path) as $file) {
+        translateFile($file, $limit, $loader, $client);
+    }
+    return;
 }
+if (is_file($path)) {
+    translateFile($path, $limit, $loader, $client);
+    return;
+}
+echo "First argument should be valid file or directory path\n";
+exit(2);
